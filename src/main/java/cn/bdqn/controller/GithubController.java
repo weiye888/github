e25ee5bbbaabfea3ce3b4cfd6c68bdd5b1685be2
未夜 <117328683+weiye888@users.noreply.github.com>
2022-11-03 15:40:25 +0800

    Github访问接口第一次修改

---
+++ src/main/java/cn/bdqn/controller/GithubController.java
@@ -0,0 +1,15 @@
+package cn.bdqn.controller;
+
+/**
+ * @ClassName: GithubController
+ * @Description: GithubController
+ * @Date: 2022-11-03 15:29:06
+ * @Author: WeiYe
+ */
+public class GithubController {
+
+    public static void main(String[] args) {
+
+    }
+
+}
